1598d21b691b0aca0e508b4bd7d692a0c1734397
ilookforme102 <ilookforme102@users.noreply.github.com>
2016-10-11 01:16:18 +0300

    Update data

--- src/PostsData.js
+++ src/PostsData.js
@@ -4,10 +4,10 @@ var posts = [
         "user": {
             "id": 1,
             "username": "dtrump",
-            "profileImageSmall": "http://core0.staticworld.net/images/article/2015/11/111915blog-donald-trump-100629006-primary.idge.jpg"
+            "profileImageSmall": "../../img/mike.png"
         },
-        "image": "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-        "imageThumbnail": "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
+        "image": "../../img/mike.png",
+        "imageThumbnail": "../../img/mike.png",
         "likes": 892,
         "caption": "Always winning #elections",
         "tags": ["elections"],
@@ -39,10 +39,10 @@ var posts = [
         "user": {
             "id": 2,
             "username": "HillaryC",
-            "profileImageSmall": "https://pbs.twimg.com/profile_images/750300510264107008/G8-PA5KA.jpg"
+            "profileImageSmall":"../../img/mike.png"
         },
-        "image": "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
-        "imageThumbnail": "http://media1.fdncms.com/sacurrent/imager/u/original/2513252/donald_trump4.jpg",
+        "image": "../../img/mike.png",
+        "imageThumbnail": "../../img/mike.png",
         "likes": 1100,
         "caption": "Loser #elections",
         "tags": ["elections"],
